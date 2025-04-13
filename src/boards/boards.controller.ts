f7ca4629df6b9ddde2fc3158f4a91d7004874335
Byungik Hyun <byungikhyun@Byungiks-MacBook-Pro.local>
2025-04-13 22:48:08 +0530

    implemented basic CRUD with typeOrm

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,8 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.entity';
+import { BoardStatus } from './board-status.enum';
+import { CreateBoardDto } from './dto/CreateBoard';
+import { BoardStatusValidationPipe } from './pipes/BoardStatusValidationPipe';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) {};
-    
+
+    @Get('/')
+    getAllBoards(): Promise<Board[]> {
+        return this.boardsService.getAllBoards();
+    }
+
+    @Post('/')
+    @UsePipes(ValidationPipe)
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto
+    ): Promise<Board> {
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id', ParseIntPipe) id: number): Promise<Board> {
+        return this.boardsService.findBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        return this.boardsService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id', ParseIntPipe) id: number,
+        @Body('status', BoardStatusValidationPipe) status: BoardStatus 
+    ): Promise<Board> {
+        return this.boardsService.updateBoardStatus(id, status);
+    }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
+import { BoardStatus } from './board-status.enum';
+import { CreateBoardDto } from './dto/CreateBoard';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    constructor(
+        @InjectRepository(BoardRepository)
+        private boardRepository: BoardRepository) {}
+
+    async getAllBoards(): Promise<Board[]> {
+        return this.boardRepository.find();
+    }
+
+    createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+        return this.boardRepository.createBoard(createBoardDto);
+    }
+
+    async findBoardById(id: number): Promise<Board> {
+        const found = await this.boardRepository.findOne({ where: { id } });
+        if (!found) {
+            throw new NotFoundException(`Board with ID "${id}" not found`);
+        }
+        return found;
+    }
+
+    async deleteBoard(id: number): Promise<void> {
+        const result = await this.boardRepository.delete(id);
+        
+        if (result.affected === 0) {
+            throw new NotFoundException(`Board with ID "${id}" not found`);
+        }
+    }
+
+    async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
+        const board = await this.findBoardById(id);
+        board.status = status;
+        await this.boardRepository.save(board);
+        return board;
+    }
+} 
